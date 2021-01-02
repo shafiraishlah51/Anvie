641d3a6e7af4f85e828eb92871e7c14c8443afe4
shafiraishlah51 <shafiraish5@gmail.com>
2021-01-02 14:42:50 +0700

    add opengl with sensor

---
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/shafira_ishlah/anvie/views/OpenGLView.java
@@ -0,0 +1,29 @@
+package id.ac.ui.cs.mobileprogramming.shafira_ishlah.anvie.views;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+
+public class OpenGLView extends GLSurfaceView {
+
+    public boolean GLReady = false;
+    public OpenGLRenderer renderer;
+
+    public OpenGLView(Context context) {
+        super(context);
+        init();
+    }
+
+    public OpenGLView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init(){
+        setEGLContextClientVersion(2);
+        setPreserveEGLContextOnPause(true);
+        renderer = new OpenGLRenderer();
+        setRenderer(renderer);
+        GLReady = true;
+    }
+}
--- app/src/main/java/id/ac/ui/cs/mobileprogramming/shafira_ishlah/anvie/views/SeeRingtoneActivity.java
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/shafira_ishlah/anvie/views/SeeRingtoneActivity.java
@@ -4,6 +4,10 @@ package id.ac.ui.cs.mobileprogramming.shafira_ishlah.anvie.views;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -12,16 +16,31 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.opengl.GLSurfaceView;
+
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+
 import id.ac.ui.cs.mobileprogramming.shafira_ishlah.anvie.R;
 
-public class SeeRingtoneActivity extends Activity implements View.OnClickListener {
+public class SeeRingtoneActivity extends Activity implements View.OnClickListener, SensorEventListener {
 
     //button objects
     private Button buttonStart;
     private Button buttonStop;
     private MusicView musicView;
+    private OpenGLView openGLView;
+    private TextView outPut1;
+    private SensorManager mSensorManager;
+    private Sensor accelerometer;
+    private Sensor magnetometer;
+    private final static int OVERFLOW_LIMIT = 20;
+    private final static int SENSITIVITY = 4;
+    private float[][] movingAverage = new float[3][OVERFLOW_LIMIT];
+    private int overflow = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +56,16 @@ public class SeeRingtoneActivity extends Activity implements View.OnClickListene
         buttonStop.setOnClickListener(this);
 
         musicView =(MusicView) findViewById(R.id.musicView);
+
+        openGLView = findViewById(R.id.openGLView);
+        outPut1 = findViewById(R.id.accText);
+
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        initListeners();
 
     }
 
@@ -73,12 +102,65 @@ public class SeeRingtoneActivity extends Activity implements View.OnClickListene
     protected void onResume() {
         super.onResume();
         musicView.onResume();
+        openGLView.onResume();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         musicView.onPause();
+        openGLView.onPause();
+    }
+
+    public void initListeners()
+    {
+        mSensorManager.registerListener((SensorEventListener) this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener((SensorEventListener) this, magnetometer, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (openGLView.GLReady) {
+            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                float v1 = Math.round(event.values[0] * 100.0) / 100f;
+                float v2 = Math.round(event.values[1] * 100.0) / 100f;
+                float v3 = Math.round(event.values[2] * 100.0) / 100f;
+
+                movingAverage[0][overflow] = v1;
+                movingAverage[1][overflow] = v2;
+
+                float s1 = calculateAverage(movingAverage[0]);
+                float s2 = calculateAverage(movingAverage[1]);
+
+                String eventValueString = String.format("a:%f b:%f c:%f", v1, v2, v3);
+                outPut1.setText("accelerometer " + eventValueString);
+                if (openGLView.renderer.objectsReady) {
+                    openGLView.renderer.getCircle().CalculatePoints(s1 / SENSITIVITY, s2 / SENSITIVITY, 0.1f, 55);
+                    openGLView.requestRender();
+                }
+            }
+            openGLView.requestRender();
+        }
+        overflow += 1;
+        if (overflow >= OVERFLOW_LIMIT) {
+            overflow = 0;
+        }
+    }
+
+    private float calculateAverage(float[] input) {
+        DoubleStream io = IntStream.range(0, input.length)
+                .mapToDouble(i -> input[i]);
+        float sum = (float)io.sum();
+        return sum/OVERFLOW_LIMIT;
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
 
